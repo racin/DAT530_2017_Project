5e3ecd7558bfe9fde7c3a5bbcab2151004276c48
Racin Nygaard <racin.nygaard@gmail.com>
2017-10-07 16:42:25 +0200

    rename

--- GPenSIM/tDealer_pre.m
+++ GPenSIM/tDealer_pre.m
@@ -3,10 +3,10 @@ function [fire, transition] = tDealer_pre(transition)
 global global_info;
 
 
-if ~isempty(global_info.UNDEALT_DECK),
-    card = randi([1,length(global_info.UNDEALT_DECK)]);
-    transition.new_color = global_info.UNDEALT_DECK(card);
-    global_info.UNDEALT_DECK(card) = []; % Remove this card from the array
+if ~isempty(global_info.DECK),
+    card = randi([1,length(global_info.DECK)]);
+    transition.new_color = global_info.DECK(card);
+    global_info.DECK(card) = []; % Remove this card from the array
     fire = 1;
     return;
 end;
