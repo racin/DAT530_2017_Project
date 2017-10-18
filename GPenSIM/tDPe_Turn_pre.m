490283a1d8c65b71ab3909c33719e4f584924f1e
Racin Nygaard <racin.nygaard@gmail.com>
2017-10-18 11:51:25 +0200

    Queue. Wrong direction. Need extra place

--- GPenSIM/tDPe_Turn_pre.m
+++ GPenSIM/tDPe_Turn_pre.m
@@ -5,18 +5,15 @@ global global_info;
 % Check if token is without color
 if is_enabled('tDPe_Turn'),
     disp('Turn enabled!')
-    %disp(struct2array(get_current_colors()))
     tokens = tokIDs('pDP_Draw_Pile');
     for i = 1:length(tokens),
         colors = get_color('pDP_Draw_Pile',tokens(i));
         disp(colors);
     end
-    trans = tokenArrivedLate('pDP_Draw_Pile',1);
-    %trans{'time'} = 1.5; HOW TO RESET TO FRONT OF QUEUE? INTERMEDIATE
-    %TRANS?
-    transition.new_color = strcat('racin',num2str(rand()*10));
-    transition.selected_tokens = trans;
+    % NOTE: In a real game it is the LAST arrived card that is turned.
+    % However, in Petri Net, if we turned the last 
+    transition.selected_tokens = tokenArrivedEarly('pDP_Draw_Pile',1);
     fire = 1;
-else
+else 
     fire = 0;
 end;
--- GPenSIM/tPe_DP_Turn_pre.m
+++ GPenSIM/tPe_DP_Turn_pre.m
@@ -3,6 +3,11 @@ function [fire, transition] = tPe_DP_Turn_pre(transition)
 
 pause(0.01); % Halts execution in the main loop to allow to check for events.
 global global_info;
+if length(tokIDs('pDP_Draw_Pile')) == 5,
+    vistoken = tokenArrivedEarly('pDP_Draw_Pile',1);
+    set(global_info.handles.DP_StatusMsg,'String',strcat('Visible card: ',get_color('pDP_Draw_Pile',vistoken)));
+end
+
 if global_info.DP_Turn_Btn ~= false,
     disp('DP Turn clicked!');
     global_info.DP_Turn_Btn = false;
